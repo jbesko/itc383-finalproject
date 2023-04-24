ceb9e1092bc50fc3513188a68c71b79fcff9df5b
Jake Besko <124289011+jbesko@users.noreply.github.com>
2023-04-24 19:51:06 -0400

    Key Adding to character

--- itc383project/Assets/Scripts/KeyAdding.cs
+++ itc383project/Assets/Scripts/KeyAdding.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class KeyAdding : MonoBehaviour
 {
     public GameObject textBox;
+
     void OnCollisionEnter () 
     {
         textBox.GetComponent<Text>().text += "*";
