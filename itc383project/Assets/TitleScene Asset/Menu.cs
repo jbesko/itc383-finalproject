3cc573201fbfce7461f9c9f223b4cad17f1a314f
Garrett <garrettb1616@gmail.com>
2023-04-30 18:32:58 -0400

    gas
    fds

--- itc383project/Assets/TitleScene Asset/Menu.cs
+++ itc383project/Assets/TitleScene Asset/Menu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Congrats : MonoBehaviour {
+public class Menu : MonoBehaviour {
 
     public void PlayGame ()
     {
